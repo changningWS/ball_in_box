b06d894c54dee458e413c6b1361e42fcd4d96e0e
changningWS <1571875858@qq.com>
2018-06-01 00:28:38 +0800

    add simulated annealing

---
+++ ball_in_box/funcTransfer.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+def funcTransfer(state, beginT, T, endT):
+    
+    """
+    change the state randomly
+    """
+    nxts = []
+    for pt in state:
+        radius = random.random() * 1.0
+        angle = random.random() * math.pi * 2.0
+        nxts.append( (pt[0] + radius*math.cos(pt[0] + angle),
+            pt[1] + radius*math.sin(pt[1] + angle),
+            max(0, pt[2] + radius*2 - 1)) )
+    
+    return nxts
